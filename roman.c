cb5b34253c591a3c32e969fac003a268990e01de
10AKasH10 <88546556+10AKasH10@users.noreply.github.com>
2021-10-27 21:32:01 +0530

    Add files via upload

--- roman.c
+++ roman.c
@@ -1,111 +1,204 @@
-#include <stdio.h>
-long tobinary(int);
-int main(){
-   int num;
-   long bno;
-    printf("Enter a decimal number: ");
-    scanf("%d", &num);
-    printf("unary number is:");
-for(int i=0;i<num;i++){
-        printf("I");
-    }
-        
-     bno = tobinary(num);
-    
-     printf("\nRoman numerals: ");    
-    while(num != 0)
+#include<stdio.h>
+#include<string.h>
+
+int decitoroman(long int num)
+{
+    if(num<=0)
     {
-
-        if (num >= 1000)       
-        {
-           printf("m");
-           num -= 1000;
-        }
-
-        else if (num >= 900)   
-        {
-           printf("cm");
-           num -= 900;
-        }        
-
-        else if (num >= 500)   
-        {           
-           printf("d");
-           num -= 500;
-        }
-
-        else if (num >= 400)   
-        {
-           printf("cd");
-           num -= 400;
-        }
-
-        else if (num >= 100)   
-        {
-           printf("c");
-           num -= 100;                       
-        }
-
-        else if (num >= 90)    
-        {
-           printf("xc");
-           num -= 90;                                              
-        }
-
-        else if (num >= 50)    
-        {
-           printf("l");
-           num -= 50;                                                                     
-        }
-
-        else if (num >= 40)    
-        {
-           printf("xl");           
-           num -= 40;
-        }
-
-        else if (num >= 10)    
-        {
-           printf("x");
-           num -= 10;           
-        }
-
-        else if (num >= 9)     
-        {
-           printf("ix");
-           num -= 9;                         
-        }
-
-        else if (num >= 5)     
-        {
-           printf("v");
-           num -= 5;                                     
-        }
-
-        else if (num >= 4)     
-        {
-           printf("iv");
-           num -= 4;                                                            
-        }
-
-        else if (num >= 1)     
-        {
-           printf("i");
-           num -= 1;                                                                                   
-        }
-
+        return -1;
     }
-   printf("\nThe Binary value is : %ld\n",bno);
-    
-    return 0;
+    while(num!=0)
+    {
+        if(num>=1000)
+        {
+            printf("M");
+            num-=1000;
+        }
+        else if(num>=900)
+        {
+            printf("CM");
+            num-=900;
+        }
+        else if(num>=500)
+        {
+            printf("D");
+            num-=500;
+        }
+        else if(num>=400)
+        {
+            printf("CD");
+            num-=400;
+        }
+        else if(num>=100)
+        {
+            printf("C");
+            num-=100;
+        }
+        else if(num>=90)
+        {
+            printf("XC");
+            num-=90;
+        }
+        else if(num>=50)
+        {
+            printf("L");
+            num-=50;
+        }
+        else if(num>=40)
+        {
+            printf("XL");
+            num-=40;
+        }
+        else if(num>=10)
+        {
+            printf("X");
+            num-=10;
+        }
+        else if(num>=9)
+        {
+            printf("IX");
+            num-=9;
+        }
+        else if(num>=5)
+        {
+            printf("V");
+            num-=5;
+        }
+        else if(num>=4)
+        {
+            printf("IV");
+            num-=4;
+        }
+        else if(num>=1)
+        {
+            printf("I");
+            num-=1;
+        }
+    }
+    return 1;
 }
-long tobinary(int num){
-   long bno=0,sem,f=1;
-   while(num != 0){
-      sem = num % 2;
-      bno = bno + sem * f;
-      f = f * 10;
-      num = num / 2;
-   }
-   return bno;;
+
+int digit(char ch)
+{
+    int val=0;
+    switch (ch)
+    {
+    case 'I':
+        val=1;
+        break;
+    case 'V':
+        val=5;
+        break;
+    case 'X':
+        val=10;
+        break;
+    case 'L':
+        val=50;
+        break;
+    case 'C':
+        val=100;
+        break;
+    case 'D':
+        val=500;
+        break;
+    case 'M':
+        val=1000;
+        break;
+    case '\0':
+        val=0;
+        break;
+    default:
+        val=-1;
+    }
+    return val;
+}
+
+long int romantodeci(char roman[])
+{
+    int i=0;
+    long int num=0;
+    while(roman[i])
+    {
+        if(digit(roman[i])<0)
+        {
+            printf("Invalid roman literal");
+            return -1;
+        }
+        if((strlen(roman)-1)>2)
+        {
+            if(digit(roman[i])<digit(roman[i+2]))
+            {
+                printf("Invalid roman literal");
+                return -1;
+            }
+        }
+        if(digit(roman[i])>=digit(roman[i+1]))
+        {
+            num+=digit(roman[i]);
+        }
+        else
+        {
+            num+=digit(roman[i+1])-digit(roman[i]);
+            i++;
+        }
+        i++;
+    }
+    return num;
+}
+
+
+
+int main()
+{
+while(1)
+    {
+     int choice;
+        printf("Select a choice: \n 1.Addition \n 2.Multiplication \n 3.Exit \n");
+        printf("Enter a choice: ");
+        scanf("%d",&choice);
+        if(choice==1)
+        {
+            char romannum1[1000],romannum2[1000];
+            printf("Enter two roman number: ");
+            scanf("%s %s", romannum1,romannum2);
+            long int decimalnum1=romantodeci(romannum1);
+            long int decimalnum2=romantodeci(romannum2);
+            if(decimalnum1==-1 || decimalnum2==-1)
+            {
+                break;
+            }
+            else
+            {
+                long int sum=decimalnum1+decimalnum2;
+                printf("Sum is: ");
+                decitoroman(sum);
+                printf("\n");
+            }
+        }
+        else if(choice==2)
+        {
+            char romannum1[1000],romannum2[1000];
+            printf("Enter two roman numeral: ");
+            scanf("%s %s",romannum1,romannum2);
+            long int decimalnum1=romantodeci(romannum1);
+            long int decimalnum2=romantodeci(romannum2);
+            if(decimalnum1==-1 || decimalnum2==-1)
+            {
+                break;
+            }
+            else
+            {
+                long int product=decimalnum1*decimalnum2;
+                printf("Product is: ");
+                decitoroman(product);
+                printf("\n");
+            }
+        }
+        else
+        {
+            printf("Exit");
+            break;
+        }
+    }
+    return 0;
 }
